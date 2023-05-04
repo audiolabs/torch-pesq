d40c799968fcf0ddb69beef4e8c8f3218307efbe
Lorenz Schmidt <bytesnake@mailbox.org>
2023-05-04 11:49:47 +0200

    Use warnings module to emit warning

--- torch_pesq/__init__.py
+++ torch_pesq/__init__.py
@@ -5,10 +5,11 @@ from .loss import PesqLoss
 # emit warning for torchaudio < 2.0.0
 import torchaudio
 from packaging import version
+import warnings
 
 if version.parse(torchaudio.__version__) < version.parse("2.0.0"):
-    print(
-        "Warning: your torchaudio seems to be older than version 2.0.0, the energy alignment routine may run slowly. See https://github.com/audiolabs/torch-pesq/issues/3 for further details."
+    warnings.warn(
+        "Your torchaudio seems to be older than version 2.0.0, the energy alignment routine may run slowly. See https://github.com/audiolabs/torch-pesq/issues/3 for further details."
     )
 
 __all__ = [
